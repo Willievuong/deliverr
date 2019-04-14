ea5874b0f2e456d6741288dec83438b41da0dad0
Stanley Lee <stanleylee91776@gmail.com>
2019-04-13 18:09:38 -0700

    fix

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,9 @@ var firebase= require("firebase-admin");
 
 var serviceAccount = require("./key.json");
 
+function makeid(length) {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
   for (var i = 0; i < length; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
