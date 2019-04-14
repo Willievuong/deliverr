b7cd95b31a7a1542efe2c53f7b552f22e1d85238
Stanley Lee <stanleylee91776@gmail.com>
2019-04-13 20:54:43 -0700

    push

--- backend/fbconfig.js
+++ backend/fbconfig.js
@@ -1,4 +1,5 @@
 var appId = "1783496798419122";
-var appSecret = "ebf092c390f90fb1c4440e0f3d7548fa";  
+var appSecret = "ebf092c390f90fb1c4440e0f3d7548fa";
 
-export {appId, appSecret}; 
+exports.appId = appId;
+exports.appSecret = appSecret;
